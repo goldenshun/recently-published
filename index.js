8234a7d77cc4bb103c763e65c78e3716c05edd93
Sean Connolly <sconnolly17@gmail.com>
2019-01-21 21:22:50 -0700

    Kinda sorta working

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 #!/usr/bin/env node
 const program = require('commander');
+const npm = require('npm');
+var pkg = require('./package.json');
 
 program
-  .version('0.1.0')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .version(pkg.version)
+  // .option('-p, --peppers', 'Add peppers')
+  // .option('-P, --pineapple', 'Add pineapple')
+  // .option('-b, --bbq-sauce', 'Add bbq sauce')
+  // .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
   .parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate()-100);
+
+npm.load({outfd: null}, () => {
+  Object.keys(pkg.dependencies).forEach(dependency => {
+    const version = pkg.dependencies[dependency];
+    npm.commands.view([dependency, 'time'], false, (er, tree) => {
+      const datePublished = new Date(tree[version].time[version]);
+      if (datePublished > yesterday) {
+        console.log(dependency, version, datePublished);
+      }
+    });
+  });
+});
+
+// console.log('you ordered a pizza with:');
+// if (program.peppers) console.log('  - peppers');
+// if (program.pineapple) console.log('  - pineapple');
+// if (program.bbqSauce) console.log('  - bbq');
+// console.log('  - %s cheese', program.cheese);
